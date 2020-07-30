85fd6d952ac1e842f7425de88a7565ec44165013
Shahid Mehmood <iss_shahid@yahoo.com>
2020-07-30 16:37:49 +0500

    metting scheduler

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  Inject,
+  ScheduleComponent,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  EventSettingsModel,
+  MonthAgenda,
+  TimelineViews,
+  TimelineMonth,
+} from "@syncfusion/ej2-react-schedule";
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ScheduleComponent currentView="Month">
+        <Inject
+          services={[
+            Day,
+            Week,
+            WorkWeek,
+            Month,
+            Agenda,
+            MonthAgenda,
+            TimelineViews,
+            TimelineMonth,
+          ]}
+        />
+      </ScheduleComponent>
     </div>
   );
 }
